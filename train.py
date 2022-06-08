ff28c672c4e5dd35584a2b2ef32c532632f56f7c
Hu Po <hugo.ponte@wandb.com>
2022-06-08 09:34:59 -0700

    memory utilization

--- train.py
+++ train.py
@@ -7,14 +7,14 @@ import time
 
 import platform
 import wandb
-from pynvml import nvmlInit, nvmlDeviceGetHandleByIndex, nvmlDeviceGetMemoryInfo
+from pynvml import nvmlInit, nvmlDeviceGetHandleByIndex, nvmlDeviceGetMemoryInfo, NVMLError
 
 parser = argparse.ArgumentParser(description='Mock training script for testing GPU support.')
-parser.add_argument("--project", type=str, default="launch-examples", help="W&B project")
-parser.add_argument('--gpu', nargs="+", type=int, default=0, help='Specify which GPUs to use')
+parser.add_argument("--project", type=str, default="summerhack", help="W&B project")
+parser.add_argument('--gpu', type=str, default="0", help='Comma seperated list of GPU ids to use')
 
 # How long should this fake training script run for?
-parser.add_argument("--train_time", type=int, default=5)
+parser.add_argument("--train_time", type=int, default=30)
 
 # Target steady state gpu utilization
 parser.add_argument("--target_gpu_utilization", type=float, default=0.9)
@@ -49,23 +49,28 @@ if __name__ == "__main__":
     print(f'\tPyTorch was able to find {torch.cuda.device_count()} GPUs')
     assert torch.cuda.is_available(), 'Torch was unable to find CUDA'
 
-    devices = {}
-    if len(args.gpu) > 1:
-        for _gpu in args.gpu:
-            assert 0 <= _gpu < torch.cuda.device_count(), f'Invalid GPU index {_gpu}'
-            print(f'\tUsing GPU {args.gpu}')
-            devices[_gpu] = torch.device(f'cuda:{args.gpu}')
-
     # Use NVIDIA Python bindings to get GPU information
     nvmlInit()
     nvidia_devices = {}
-    for id, device in devices:
-        _nvidia_device = nvmlDeviceGetHandleByIndex(id)
+    for id in args.gpu.split(','):
+        try:
+            _id = int(id)
+            _nvidia_device = nvmlDeviceGetHandleByIndex(_id)
+        except (NVMLError, TypeError) as e:
+            raise AssertionError(f"Error getting device handle to GPU {_id}: {e}")
         print(f'\tGPU {id} has {nvmlDeviceGetMemoryInfo(_nvidia_device).total} total memory')
         print(f'\tGPU {id} has {nvmlDeviceGetMemoryInfo(_nvidia_device).free} free memory')
         print(f'\tGPU {id} has {nvmlDeviceGetMemoryInfo(_nvidia_device).used} used memory')
-        nvidia_devices[id] = _nvidia_device
-    
+        nvidia_devices[_id] = _nvidia_device
+    assert len(nvidia_devices) > 0, 'No NVIDIA GPUs found'
+
+    # Register devices with PyTorch
+    devices = {}
+    for id in nvidia_devices.keys():
+        assert 0 <= id < torch.cuda.device_count(), f'Invalid GPU index {id}'
+        devices[id] = torch.device(f'cuda:{id}')
+        print(f'\tPytorch: created device using GPU {id}')
+
     # Increase size of tensor over time until max GPU memory utilization
     tensor_size = {id: 2 for id in devices.keys()}
 
@@ -79,7 +84,7 @@ if __name__ == "__main__":
         wandb.log({"time.remaining": time_remaining})
         wandb.log({"time.now": time.time()})
 
-        for id, device in devices:
+        for id, device in devices.items():
             _tensor_size = int(tensor_size[id])
             a = torch.randn(_tensor_size, _tensor_size).to(device)
             b = torch.randn(_tensor_size, _tensor_size).to(device)
@@ -87,10 +92,10 @@ if __name__ == "__main__":
             _used = nvmlDeviceGetMemoryInfo(_nvidia_device).used
             _total = nvmlDeviceGetMemoryInfo(_nvidia_device).total
             utilization = _used / _total
-            wandb.log({f"gpu.utilization.{id}": utilization})
-            print(f'\t\tGPU {id} Utilization at {utilization}')
+            wandb.log({f"gpu.mem.utilization.{id}": utilization})
+            print(f'\t\tGPU {id} Memory Utilization at {utilization}')
             if utilization < args.target_gpu_utilization:
-                tensor_size[id] *= 1.03
+                tensor_size[id] *= 1.07
             print(f'\t\t A ({_tensor_size},{_tensor_size}) x B ({_tensor_size},{_tensor_size})')
 
     wandb.finish()
